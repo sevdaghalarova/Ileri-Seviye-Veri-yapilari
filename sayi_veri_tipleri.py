c110e2ea05653c1d156e797214dcb5d6264843a5
sevdaghalarova <51816139+sevdaghalarova@users.noreply.github.com>
2020-10-10 00:14:43 +0400

    sayalar uzerinde ileri fonksiyonlar

---
+++ sayi_veri_tipleri.py
@@ -0,0 +1,8 @@
+print(bin(20)) # 10'luk tabandaki veriyi 2'li tabana cevirir
+print(hex(20)) # 10'luk tabandaki veriyi 16'lik tabana cevirir
+print(abs(-1)) # mutlak deger alir
+print(round(3.7)) # alta  veya uste yuvarlar
+print(max(1,2,5,7,3,1,9,4)) # max degeri dondurur
+print(min(-1,5,-4,2,6)) # min degeri dondurur
+print(sum([1,5,3,5,3])) # itarable deger alir, ve sonuc olarak onlarin toplamini verir
+print(pow(2,3)) # 2**3 ile ayni sonucu verir 2 uzeri 3
